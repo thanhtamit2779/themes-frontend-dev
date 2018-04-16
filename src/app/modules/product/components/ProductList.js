9d9b53572e38cea88950f97009b83d3990ec7d18
ThanhTam <thanhtam.it2779@gmail.com>
2018-04-16 08:32:02 +0700

    Add to cart

--- src/app/modules/product/components/ProductList.js
+++ src/app/modules/product/components/ProductList.js
@@ -3,17 +3,43 @@ import React, {Component} from 'react';
 import { NavLink } from 'react-router-dom';
 import * as _ from 'lodash';
 
+const localStorageCarts = JSON.parse(localStorage.getItem('shoppingCart'));
+const carts             = ( _.isEmpty(localStorageCarts) ) ? [] : localStorageCarts;
+
 class ProductList extends Component {
     constructor(props) {
         super(props);
+
+        this.findProductInCart = this.findProductInCart.bind(this);
     }
 
-    handleAddCart(product_id, product) {
-        let cart = {};
-        cart[product_id] = product;
-
-        localStorage.setItem('cart', cart);
+    findProductInCart(cart, product) {
+        var index = -1;
+        if (cart.length > 0) {
+            for (var i = 0; i < cart.length; i++) {
+                if (cart[i].product.post_id === product.post_id) {
+                    index = i;
+                    break;
+                }
+            }
+        }
+        return index;
     }
+
+    handleAddCart(product) {
+        let index = this.findProductInCart(carts, product);
+        if (index !== -1) {
+            carts[index].quantity += 1;
+        } else {
+            let quantity = 1;
+            carts.push({
+                product,
+                quantity
+            });
+        }
+        localStorage.setItem('shoppingCart', JSON.stringify(carts));
+    }
+
 
     load(posts, col = '') {
         if (_.isEmpty(posts)) 
@@ -25,13 +51,13 @@ class ProductList extends Component {
         return posts.map((post, key) => {
             let post_id     = post.post_id;
             let post_title  = post.post_title;
+            let post_thumb  = post.post_thumb;
             let link_detail = `/chi-tiet/${post.post_slug}/${post_id}`;
 
             let set_cart    = {
                 'post_id'           : post_id,
                 'post_title'        : post_title,
-                'post_thumbnail'    : post.post_thumb,
-                'post_quantity'     : 1        
+                'post_thumbnail'    : post_thumb  
             }
 
             return (
@@ -42,7 +68,7 @@ class ProductList extends Component {
                             <div className="block2-overlay trans-0-4">
                                 <div className="block2-btn-addcart w-size1 trans-0-4">
                                     {/* Button */}
-                                    <button className="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4" onClick={ () => this.handleAddCart(post_id, set_cart)}>
+                                    <button className="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4" onClick={ () => this.handleAddCart(post) }>
                                         ĐẶT MUA
                                     </button>
                                 </div>
