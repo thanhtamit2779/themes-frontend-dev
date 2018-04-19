1156afd724e576df60b20d82fb73faef3f7e5880
ThanhTam <thanhtam.it2779@gmail.com>
2018-04-19 13:30:00 +0700

    Update menu

--- src/app/modules/config/menu.js
+++ src/app/modules/config/menu.js
@@ -1,38 +1,27 @@
 const menu = [
     {
-        'icon' : 'fa fa-home',
-        'name' : 'Trang chủ',
-        'path' : '/'
+        'icon'  : 'fa fa-home',
+        'name'  : 'Trang chủ',
+        'path'  : '/',
+        'click' : false,
     },
     {
         'icon' : 'fa fa-product-hunt',
         'name' : 'Sản phẩm',
-        'path' : '/san-pham'
+        'path' : '/#',
+        'click' : true,
     },
     {
         'icon' : 'fa fa-info',
         'name' : 'Giới thiệu',
-        'path' : '/gioi-thieu'
+        'path' : '/gioi-thieu',
+        'click' : false,
     },
     {
         'icon' : 'fa fa-envelope',
         'name' : 'Liên hệ',
-        'path' : '/lien-he'
-    },
-    // {
-    //     'icon' : 'fa fa-user',
-    //     'name' : 'Tài khoản',
-    //     'path' : '/tai-khoan'
-    // },
-    // {
-    //     'icon' : 'fa fa-phone',
-    //     'name' : '028.7300.4488',
-    //     'path' : 'tel:02873004488'
-    // },
-    // {
-    //     'icon' : 'fa fa-envelope',
-    //     'name' : 'sales@webdoctor.vn',
-    //     'path' : 'mailto:sales@webdoctor.vn'
-    // }
+        'path' : '/lien-he',
+        'click' : false,
+    }
 ];
 export default menu ;
