e9297b76cf979d92613886061d4cdc8b57d8d03d
ThanhTam <thanhtam.it2779@gmail.com>
2018-04-25 08:23:52 +0700

    Css v1

--- src/app/config/api.js
+++ src/app/config/api.js
@@ -1,5 +1,5 @@
 const API = { 
-    url         : 'http://themes.dev.vn/api/',
-    admin_url   : 'http://themes.dev.vn/admin/'
+    url         : 'http://localhost/themes.webdoctor.vn/api/',
+    admin_url   : 'http://localhost/themes.webdoctor.vn/admin/'
 };
 export default API;
