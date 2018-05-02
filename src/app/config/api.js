cd51303f73e20b98fc4c25dba59c45e7f1225797
ThanhTam <thanhtam.it2779@gmail.com>
2018-05-02 20:21:31 +0700

    Config link API

--- src/app/config/api.js
+++ src/app/config/api.js
@@ -1,5 +1,11 @@
+// const API = { 
+//     url         : 'http://thanhtamit.freevnn.com/themes/api/',
+//     admin_url   : 'http://thanhtamit.freevnn.com/themes/admin/'
+// };
+// export default API;
+
 const API = { 
-    url         : 'http://localhost/themes.webdoctor.vn/api/',
-    admin_url   : 'http://localhost/themes.webdoctor.vn/admin/'
+    url         : 'http://localhost/webdoctor.vn/themes/api/',
+    admin_url   : 'http://localhost/webdoctor.vn/themes/admin/'
 };
 export default API;
